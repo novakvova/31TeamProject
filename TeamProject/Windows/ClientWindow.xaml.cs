f7f754051ba96841aa24d4b222788fb68b61f775
work <work@TEST>
2019-05-08 15:33:51 +0300

    edit user, uesrModel, clientWindow

--- TeamProject/Windows/ClientWindow.xaml.cs
+++ TeamProject/Windows/ClientWindow.xaml.cs
@@ -23,28 +23,37 @@ namespace TeamProject.Windows
     /// </summary>
     public partial class ClientWindow : Window
     {
-        public string _conStr = "Data Source=karaka123.mssql.somee.com;User ID=gmirakivan_SQLLogin_1;Password=8b1m2f1gnt";
+        ///public string _conStr = "Data Source=karaka123.mssql.somee.com;User ID=gmirakivan_SQLLogin_1;Password=8b1m2f1gnt";
         private EFContext _context;
         private ObservableCollection<UserModel> usersList;
-        private List<CarModel> carsList;
+        private List<CarModel> carsList, clientsList, brokersList;
         public ClientWindow()
         {
             InitializeComponent();
             _context = new EFContext();
 
             usersList = new ObservableCollection<UserModel>();
-            usersList.Add(new UserModel() { ID = 1, FirstName = "q", LastName = "w", Email = "q@q.com", Password = "1234" });
-            usersList.Add(new UserModel() { ID = 2, FirstName = "a", LastName = "s", Email = "a@a.com" });
+            usersList.Add(new UserModel() { ID = 1, FirstName = "q", LastName = "w", Email = "q@q.com", Password = "1234", Status = "client" });
+            usersList.Add(new UserModel() { ID = 2, FirstName = "a", LastName = "s", Email = "a@a.com", Status = "broker" });
             carsList = new List<CarModel>();
             carsList.Add(new CarModel() { ID = 1, Brand = "audi", GraduationYear = "1234", VIN = "4321", StateNumber = "1234" });
             carsList.Add(new CarModel() { ID = 2, Brand = "bmw", GraduationYear = "4567", VIN = "7654", StateNumber = "4567" });
+
 
             DB_Load();
         }
 
         public void DB_Load()
         {
-            
+            //usersList = new ObservableCollection<UserModel>(
+            //    _context.Users.Select(u => new UserModel()
+            //    {
+            //        UserID = u.UserID,
+            //        FirstName = u.FirstName,
+            //        LastName = u.LastName,
+            //        Email = u.Email,
+            //        Password = u.Password
+            //    }).ToList());
             try
             {
                 //using (SqlConnection con = new SqlConnection(_conStr))
