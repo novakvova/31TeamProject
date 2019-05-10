50b3eeb19c4f80f5ffabe05e8d2d85dbb823cb0a
Sneiksus <meison543@gmail.com>
2019-05-10 18:53:55 +0300

    fixed

--- TeamProject/Excl.cs
+++ TeamProject/Excl.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TeamProject.Entities;
+using TeamProject.Models;
 
 namespace TeamProject
 {
@@ -17,7 +18,9 @@ namespace TeamProject
         static public void WriteToExcel()
         {
             EFContext _ctx = new EFContext();
-            var list =_ctx.Users.ToList();
+                List<UserModel> list = new List<UserModel>();
+                foreach(var a in _ctx.Users.ToList())
+                list.Add(new UserModel { ID = a.ID, Email = a.Email, FirstName = a.FirstName, LastName = a.LastName, Password = a.Password });
             var wb = new XLWorkbook();
             var ws = wb.Worksheets.Add("DataWorksheet");
             PropertyInfo[] properties = list.First().GetType().GetProperties();
@@ -28,8 +31,10 @@ namespace TeamProject
             }
             ws.Cell(2, 1).InsertData(list);
             wb.SaveAs(@"Users.xlsx");
-            var list2 = _ctx.Autos.ToList();
-             wb = new XLWorkbook();
+            List<CarModel> list2 = new List<CarModel>();
+            foreach (var a in _ctx.Autos.ToList())
+                list2.Add(new CarModel { ID = a.ID, Brand = a.Brand, VIN = a.VIN, GraduationYear = a.GraduationYear, StateNumber = a.StateNumber });
+            wb = new XLWorkbook();
              ws = wb.Worksheets.Add("DataWorksheet");
              properties = list2.First().GetType().GetProperties();
              headerNames = properties.Select(prop => prop.Name).ToList();
@@ -39,6 +44,19 @@ namespace TeamProject
             }
             ws.Cell(2, 1).InsertData(list2);
             wb.SaveAs(@"Cars.xlsx");
+            List<BrokerModel> list3 = new List<BrokerModel>();
+            foreach (var a in _ctx.Brokers.ToList())
+                list3.Add(new BrokerModel { ID = a.ID, Email = a.Email, FirstName = a.FirstName, LastName = a.LastName, Password = a.Password });
+            wb = new XLWorkbook();
+            ws = wb.Worksheets.Add("DataWorksheet");
+            properties = list3.First().GetType().GetProperties();
+            headerNames = properties.Select(prop => prop.Name).ToList();
+            for (int i = 0; i < headerNames.Count; i++)
+            {
+                ws.Cell(1, i + 1).Value = headerNames[i];
+            }
+            ws.Cell(2, 1).InsertData(list3);
+            wb.SaveAs(@"Brokers.xlsx");
         }
     }
 }
